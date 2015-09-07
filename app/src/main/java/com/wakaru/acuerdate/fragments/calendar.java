1412f4d0e88ba53f3f656299d6d6520343bb8c1a
diego <diego.vallejos@usach.cl>
2015-09-07 02:26:34 -0300

    add de datepicker, pero no calendar

--- app/src/main/java/com/wakaru/acuerdate/fragments/calendar.java
+++ app/src/main/java/com/wakaru/acuerdate/fragments/calendar.java
@@ -1,53 +1,112 @@
 package com.wakaru.acuerdate.fragments;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import com.wakaru.acuerdate.R;
 
 import java.util.Calendar;
-
-import android.app.DatePickerDialog;
-import android.app.DatePickerDialog.OnDateSetListener;
-import android.app.Dialog;
-import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+import java.util.TimeZone;
 
 
-public class calendar extends DialogFragment  {
+public class calendar extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
 
-    private OnDateSetListener mDateSetListener;
 
-    public calendar() {
-        // nothing to see here, move along
-    }
-
+    // TODO: Rename and change types and number of parameters
     public static calendar newInstance() {
         calendar fragment = new calendar();
         Bundle args = new Bundle();
+        fragment.setArguments(args);
         return fragment;
     }
 
-    public calendar(OnDateSetListener callback) {
-        mDateSetListener = (OnDateSetListener) callback;
+    public calendar() {
+        // Required empty public constructor
     }
 
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        Calendar cal = Calendar.getInstance();
-
-        return new DatePickerDialog(getActivity(),
-                mDateSetListener, cal.get(Calendar.YEAR),
-                cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        Calendar cal = Calendar.getInstance(TimeZone.getDefault()); // Get current date
+
+// Create the DatePickerDialog instance
+        DatePickerDialog datePicker = new DatePickerDialog(getActivity(),
+                R.style.AppBlackTheme, datePickerListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH));
+        datePicker.setCancelable(false);
+        datePicker.setTitle("Select the date");
+        datePicker.show();
 
 
 
+        return inflater.inflate(R.layout.fragment_calendar, container, false);
+    }
+
+    private DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
+
+        // when dialog box is closed, below method will be called.
+        public void onDateSet(DatePicker view, int selectedYear,
+                              int selectedMonth, int selectedDay) {
+            String year1 = String.valueOf(selectedYear);
+            String month1 = String.valueOf(selectedMonth + 1);
+            String day1 = String.valueOf(selectedDay);
+
+        }
+    };
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+
+    }
+
+   /* @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }*/
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
 
 }
